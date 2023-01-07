573cec856b7ad17c62fe27f2aa74b0b8b5c2ece0
Arina Nikitina <Arina.Nikitina@bercut.com>
2023-01-07 19:14:56 +0400

    Made parser for cian

--- src/main/java/com/telegram/bot/search/house/dto/ResponseDto.java
+++ src/main/java/com/telegram/bot/search/house/dto/ResponseDto.java
@@ -2,13 +2,21 @@ package com.telegram.bot.search.house.dto;
 
 import lombok.Data;
 
+import java.time.LocalDateTime;
+
 @Data
 public class ResponseDto {
     private String url;
     private String title;
     private String price;
-    private String district;
-    private String street;
+    private String location;
+    private String square;
     private String rooms;
     private String floor;
+    private String year;
+    private OwnerDto owner;
+    private RenovationDto renovationDto;
+    private LocalDateTime dateCreated;
+    private boolean animal;
+    private boolean kids;
 }
