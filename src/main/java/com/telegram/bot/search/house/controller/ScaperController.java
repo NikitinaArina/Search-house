d188ab02c2f5a260557b6845a5a15aa09b043300
Arina Nikitina <Arina.Nikitina@bercut.com>
2023-02-12 18:46:32 +0400

    Added database and fixes for cian and avito

--- src/main/java/com/telegram/bot/search/house/controller/ScaperController.java
+++ src/main/java/com/telegram/bot/search/house/controller/ScaperController.java
@@ -1,13 +1,10 @@
 package com.telegram.bot.search.house.controller;
 
-import com.telegram.bot.search.house.dto.ResponseDto;
 import com.telegram.bot.search.house.service.ScraperServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Set;
 
 @RestController
 @RequestMapping("/api/")
@@ -20,7 +17,7 @@ public class ScaperController {
     }
 
     @GetMapping("get")
-    public Set<ResponseDto> get() {
-        return service.getAds();
+    public void get() {
+        service.getAds();
     }
 }
---
+++ src/main/java/com/telegram/bot/search/house/entity/Ad.java
@@ -0,0 +1,36 @@
+package com.telegram.bot.search.house.entity;
+
+import com.telegram.bot.search.house.dto.OwnerDto;
+import com.telegram.bot.search.house.dto.RenovationDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Ad {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String url,
+            title,
+            location;
+    private Long price,
+            rooms,
+            floor,
+            year,
+            square;
+    @Enumerated(EnumType.STRING)
+    private OwnerDto owner;
+    @Enumerated(EnumType.STRING)
+    private RenovationDto renovationType;
+    private LocalDateTime dateCreated;
+    private boolean animal;
+    private boolean kids;
+}
