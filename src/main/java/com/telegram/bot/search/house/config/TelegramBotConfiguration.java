6c9b8a5f5030d11b5b46b48a6196047c3e8716ea
Arina Nikitina <Arina.Nikitina@bercut.com>
2023-02-17 21:26:53 +0400

    Added settings for telegram bot

---
+++ src/main/java/com/telegram/bot/search/house/config/TelegramBotConfiguration.java
@@ -0,0 +1,16 @@
+package com.telegram.bot.search.house.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
+
+@Configuration
+public class TelegramBotConfiguration {
+
+    @Bean
+    TelegramBotsApi telegramBotsApi() throws TelegramApiException {
+        return new TelegramBotsApi(DefaultBotSession.class);
+    }
+}
